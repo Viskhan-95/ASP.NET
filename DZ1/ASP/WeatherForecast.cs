9f127bb65fcefd65915488db753f9f2fc4399011
Viskhan <rustam1987@mail.ru>
2021-09-11 17:05:40 +0300

    sent DZ1

---
+++ DZ1/ASP/WeatherForecast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP
+{
+    //Создаем класс для хранения температуры и даты
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; } // Поле для хранеия даты изменения погоды
+        public double TemperatureC { get; set; } //Поле для хранения температуры
+    }
+}
